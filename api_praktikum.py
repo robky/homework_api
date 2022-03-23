4ed32a44f162842e1a3dd60c1251fa7fa7359d38
Pavel Gaidukov <mailgpn@gmail.com>
2022-03-23 21:12:15 +0500

    add .env and datetime

--- api_praktikum.py
+++ api_praktikum.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 import requests
 import os
-
+import datetime as DT
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,16 +9,18 @@ load_dotenv()
 # доступна в пространстве переменных окружения
 
 token = os.getenv('TOKEN')
+date = DT.datetime.fromisoformat('2022-03-01 00:00:00')
 
 url = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 headers = {'Authorization': f'OAuth {token}'}
-payload = {'from_date': 1549962000}
+payload = {'from_date': int(date.timestamp())}
 
 # Делаем GET-запрос к эндпоинту url с заголовком headers и параметрами params
 homework_statuses = requests.get(url, headers=headers, params=payload)
 
 # Печатаем ответ API в формате JSON
-#print(homework_statuses.text)
+# print(homework_statuses.text)
 
-# А можно ответ в формате JSON привести к типам данных Python и напечатать и его
+# А можно ответ в формате JSON привести к типам данных Python и напечатать и
+# его
 pprint(homework_statuses.json())
